fb877f096ff07d2004ad53481a2c27a465a66eea
DuongTanDat <datdtce171751@fpt.edu.vn>
2025-02-12 20:34:48 +0700

    create layout admin and index

---
+++ CV_Flare.RazorPage/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CV_Flare.RazorPage.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
